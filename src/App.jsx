e813e7a19ccf91d403b28996e39346af5e564d0c
Joshua Iruobe <joshuairuobe@gmail.com>
2025-10-15 00:30:28 +0100

    Added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,39 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState(null);
+  const[query, setQuery] = useState('');
 
   {/*Converting the api data from the url to json*/}
   const getData = () => {
-    fetch("https://api.weatherapi.com/v1/current.json?key=0c0fb78404de4442a25213712251410&q=London&aqi=no")
+    if (!query) return; 
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=0c0fb78404de4442a25213712251410&q=${query}&aqi=no`)
       .then(res => res.json()) 
       .then(data => setData(data));
+
+    setQuery('');
   }
 
   return (
     <>
       <h1>Weather App</h1>
+      <input 
+        type="text" 
+        placeholder="Please enter a city"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <button onClick={getData}>Get Weather</button>
 
       {/*Only rendering IF there is api data present*/}
       {data && 
         <div>
           <img src={data.current.condition.icon} alt="" />
-          <p>Location: {data.location.name}</p>
-          <p>Temperature: {data.current.temp_c}</p>
+          <h2>{data.location.name}, {data.location.country}</h2>
+          <h3>{data.current.condition.text}</h3>
+          <p>Temperature: {data.current.temp_c}°C</p>
+          <p>Feels like: {data.current.feelslike_c}°C</p>
+          <p>Humidity: {data.current.humidity}</p>
         </div>
       }
       
